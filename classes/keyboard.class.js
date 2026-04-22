e1d5e263be2c2ded9ec1cbaf60db2ceef5acd885
Bjoern <onoofelogmail.com>
2026-04-22 17:51:10 +0200

    - added keyboard.class.js for controls - added eventlistener

---
+++ classes/keyboard.class.js
@@ -0,0 +1,9 @@
+class Keyboard{
+
+Left = false;
+right = false;
+jump = false;
+shoot = false;
+
+}
+
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let canvas;
 let world;
+let keyboard;
 
 function init(){
     canvas = document.getElementById('game_canvas');
     world = new World(canvas);
+    keyboard = new Keyboard();
     
     // Nötig um Positionen von Elementen auf der Canvas zu steuern
         //ctx = canvas.getContext('2d');
@@ -15,3 +17,31 @@ function init(){
     
     console.log("My character is " , world.character);
 }
+
+window.addEventListener("keydown", (event) => {
+    console.log(event);
+    if (event.code === 'a' || event.code === 'A') {
+        Keyboard.left = true;
+    } else if (event.code === 'd' || event.code === 'D') {
+        Keyboard.right = true;
+    } else if (event.code === 'Space') {
+        Keyboard.jump = true;
+        console.log(Keyboard.jump);
+    } else if (event.code === 'Enter') {
+        Keyboard.shoot = true;
+    }
+});
+
+window.addEventListener("keyup", (event) => {
+    console.log(event);
+    if (event.code === 'a' || event.code === 'A') {
+        Keyboard.left = false;
+    } else if (event.code === 'd' || event.code === 'D') {
+        Keyboard.right = false;
+    } else if (event.code === 'Space') {
+        Keyboard.jump = false;
+        console.log(Keyboard.jump);
+    } else if (event.code === 'Enter') {
+        Keyboard.shoot = false;
+    }
+});
