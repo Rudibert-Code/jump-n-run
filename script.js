e6a9fb137635a8b5b7f69bc18eac9a52f4e351a6
Bjoern <onoofelogmail.com>
2026-05-08 16:45:03 +0200

    - added start button

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 let canvas;
 let world;
 let keyboard;
+
 function init(){
     canvas = document.getElementById('game_canvas');
     keyboard = new Keyboard();
     world = new World(canvas, keyboard);    
-    console.log("My character is " , world.character);
 }
 window.addEventListener("keydown", (event) => {
-    //console.log(event);
+    console.log(event);
     if (event.keyCode == 65) {
         keyboard.left = true;
     } else if (event.keyCode == 68) {
         keyboard.right = true;
-    } else if (event.code == 'Space' && world.character.position_y >= 400) {
+    } else if (event.keyCode == 87 && world.character.position_y >= 400) {
         keyboard.jump = true;
-    } else if (event.code == 'Enter') {
+    } else if (event.keyCode == 79) {
         keyboard.shoot = true;
     }
 });
@@ -25,9 +25,9 @@ window.addEventListener("keyup", (event) => {
         keyboard.left = false;
     } else if (event.keyCode == 68) {
         keyboard.right = false;
-    } else if (event.code == 'Space') {
+    } else if (event.keyCode == 87) {
        keyboard.jump = false;
-    } else if (event.code == 'Enter') {
+    } else if (event.keyCode == 79) {
         keyboard.shoot = false;
     }
 });
