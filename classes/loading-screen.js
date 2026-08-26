f593eef3858384091eb1c12bd90019b0cba8c31a
Bjoern <onoofelogmail.com>
2026-08-26 20:27:08 +0200

    added eventlistener to check readystate & hide loadingscreen once assets are loaded

---
+++ classes/loading-screen.js
@@ -0,0 +1,14 @@
+function closeLoadingScreen(){
+    document.getElementById('loading').classList.remove("show");
+}
+
+async function waitForAssets(){
+    if (document.readyState != 'complete') {
+        await new Promise((resolve) => {
+            window.addEventListener('load', resolve, { once: true });
+        });
+    }
+    closeLoadingScreen();
+}
+
+waitForAssets();
