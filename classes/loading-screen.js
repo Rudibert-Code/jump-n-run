f01aee0a0cbc62151a0221f67e62874493ad16ea
Bjoern <onoofelogmail.com>
2026-09-09 15:32:19 +0200

    added jsdoc to loading-screen.js

--- classes/loading-screen.js
+++ classes/loading-screen.js
@@ -1,11 +1,20 @@
+/**
+ * Make loading screen visible
+ */
 function openLoadingScreen(){
     document.getElementById('loading').classList.add("show");
 }
 
+/**
+ * Make loading screen invisible
+ */
 function closeLoadingScreen(){
     document.getElementById('loading').classList.remove("show");
 }
 
+/**
+ * Wait for level elements to be loaded; The trigger closeLoadingScreen() if elements are loaded
+ */
 async function waitForAssets(){
     if (document.readyState != 'complete') {
         await new Promise((resolve) => {
@@ -15,4 +24,7 @@ async function waitForAssets(){
     closeLoadingScreen();
 }
 
+/**
+ * Trigger waitForAssets()
+ */
 waitForAssets();
