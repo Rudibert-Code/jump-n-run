66babdb0fc63e537c71bd02445c8a9212a7ad6eb
Bjoern <onoofelogmail.com>
2026-09-09 13:22:18 +0200

    added jsdoc to BG1.class.js

--- classes/BG1.class.js
+++ classes/BG1.class.js
@@ -1,7 +1,16 @@
+/**
+ * Create new background element (layer1)
+ */
 class BG1 extends MovableObject{
     height = 256;
     width = 800;
 
+    /**
+     * Load and position background element.
+     * @param {string} imgPath 
+     * @param {number} x 
+     * @param {number} y 
+     */
     constructor(imgPath, x, y){
         super().loadImage(imgPath);
 
