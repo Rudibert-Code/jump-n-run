660774bcbec8580d0bc461a583331b31ed733a44
Bjoern <onoofelogmail.com>
2026-09-09 14:36:43 +0200

    - refactoring - added jsdoc to level.class.js

--- classes/level.class.js
+++ classes/level.class.js
@@ -1,3 +1,6 @@
+/**
+ * Create new level elements
+ */
 class Level {
     levelTheme;
     enemiesTank;
@@ -15,6 +18,22 @@ class Level {
 
     levelEndX = 3300;
 
+    /**
+     * Load level elements
+     * @param {string} levelTheme - level audio
+     * @param {Array} enemiesTank - Enemy Tank
+     * @param {Array} enemiesHover - Enemy Hover
+     * @param {Array} enemiesBoss - Boss
+     * @param {Array} levelDeko - Level deko
+     * @param {Array} sky - Sky bg element
+     * @param {Array} backgroundElements1 - BG1 bg element
+     * @param {Array} backgroundElements2 - BG2 bg element
+     * @param {Array} platforms - Platforms level element
+     * @param {Array} foregroundElement - FG bg element
+     * @param {Array} coins - Coin collectible element 
+     * @param {Array} amo - Amo collectible element
+     * @param {Array} lava - Hazardous level element
+     */
     constructor(levelTheme, enemiesTank, enemiesHover, enemiesBoss, levelDeko, sky, backgroundElements1, backgroundElements2, platforms, foregroundElement, coins, amo, lava){
         this.levelTheme = levelTheme;
         this.enemiesTank = enemiesTank;
