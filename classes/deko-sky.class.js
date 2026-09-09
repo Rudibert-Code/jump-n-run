53f98cf1cf87db07d7fff32edc0a4a79aaf9f720
Bjoern <onoofelogmail.com>
2026-09-09 13:46:55 +0200

    added jsdoc to deko-sky.class.js

--- classes/deko-sky.class.js
+++ classes/deko-sky.class.js
@@ -1,17 +1,27 @@
+/**
+ * Create new Sky element
+ */
 class DekoSky extends MovableObject{
     devi = Math.random() * 30;
     height = 200 - this.devi;
     width = 800 - this.devi;
 
-        constructor(imgPath, x, y){
+    /**
+     * 
+     * @param {string} imgPath - image path
+     * @param {number} x - position on x-axes
+     * @param {number} y - position on y-axes
+     */
+    constructor(imgPath, x, y){
         super().loadImage(imgPath);
-
         this.position_x = x;
         this.position_y = y;
-
         this.animate();
     }
 
+    /**
+     * Update element position on x-axes at set interval
+     */
     animate(){
         setInterval( () => {
             this.position_x --;
