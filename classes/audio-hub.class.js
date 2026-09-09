c3ae7a1fb1b8bd06a9b8616cd5489a04a12110a9
Bjoern <onoofelogmail.com>
2026-09-09 13:20:39 +0200

    added jsdoc to audio-hub.class.js

--- classes/audio-hub.class.js
+++ classes/audio-hub.class.js
@@ -1,3 +1,6 @@
+/**
+ * Creates new audio sounds.
+ */
 class AudioHub {
     static Walk = new Audio('./assets/sounds/sound_walk.wav');
     static Shot = new Audio('./assets/sounds/sound_laser_hero.wav');
@@ -6,12 +9,20 @@ class AudioHub {
     static Hit = new Audio('./assets/sounds/sound_hit.wav');
     static UISelect = new Audio('./assets/sounds/sound_select.wav');
 
+    /**
+     * Controls sound volume and play time. 
+     * @param {string} sound - Get sound effect via link. 
+     */
     static playSound(sound) {
         sound.volume = effectVolume;
         sound.currentTime = 0;
         sound.play();
     }
 
+    /**
+     * Pauses sound effect.
+     * @param {string} sound 
+     */
     static stopSound(sound) {
         sound.pause();
     }
