fd9dde84f511c1ac619fa20052954980c3def8a9
Bjoern <onoofelogmail.com>
2026-09-09 14:06:48 +0200

    - refactoring - added fsdoc to enemy-hover.class.js

--- classes/enemy-hover.class.js
+++ classes/enemy-hover.class.js
@@ -1,3 +1,6 @@
+/**
+ * Create new Enemy Hover element
+ */
 class EnemyHover extends MovableObject{
     height = 300;
     width = 300;
@@ -8,6 +11,11 @@ class EnemyHover extends MovableObject{
     hitHeight = 120;
     pause = "false";
 
+    /**
+     * 
+     * @param {string} imgPath 
+     * @param {number} x 
+     */
     constructor(imgPath, x){
         super().loadImage(imgPath);
         this.position_x = x;
@@ -15,17 +23,26 @@ class EnemyHover extends MovableObject{
         this.checkForPause();
     }
 
+    /**
+     * Check if game is paused
+     */
     checkForPause(){
         setInterval(() => {
             this.pause = localStorage.getItem("paused");
         }, 1000/30);
     }
 
+    /**
+     * Move Enemy Hover up
+     */
     animateUp(){
         this.aniUp = setInterval( () => {
             if (this.pause == "false") {
-                
                 this.position_y-=2;
+
+                /**
+                 * Trigger Enemy Hover's "Down" animation
+                 */
                 if (this.position_y == 150) {
                     this.animateDown();
                     clearInterval(this.aniUp);
@@ -35,11 +52,18 @@ class EnemyHover extends MovableObject{
             }
         }, 1000 / 30)
     }
+
+    /**
+     * Move Enemy over down
+     */
     animateDown(){
         this.aniDown = setInterval( () => {
             if (this.pause == "false") {
-
                 this.position_y+=2;
+
+                /**
+                 * Trigger Enemy Hover's "Up" animation
+                 */
                 if (this.position_y == 350) {
                     this.animateUp();
                     clearInterval(this.aniDown);
@@ -49,6 +73,10 @@ class EnemyHover extends MovableObject{
             }
         }, 1000 / 30)
     }
+
+    /**
+     * Update Enemy Hover's position on y-axes
+     */
     setPosition(){
         this.hitOffset_x = this.position_x + 100;
         this.hitOffset_y = this.position_y + 85;
